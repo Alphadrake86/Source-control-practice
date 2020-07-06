760724817baaf6c09670fd3c2bf11d25763149b1
Lunaris <aerik0712@gmail.com>
2020-07-06 10:39:32 -0700

    Create Class1.cs
    added this random class thingy

---
+++ Class1.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Source_control_practice
+{
+    class Class1
+    {
+        public int number { get; }
+        public Class1(int seed)
+        {
+            number = seed * new DateTime().Millisecond;
+        }
+    }
+}
