9fa122c1b4a9c252c1750d8d244cc1b9a9b688e2
Lunaris <aerik0712@gmail.com>
2020-07-06 10:31:55 -0700

    Update Program.cs
    removed "world"

--- Program.cs
+++ Program.cs
@@ -6,7 +6,7 @@ namespace Source_control_practice
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello!");
         }
     }
 }
